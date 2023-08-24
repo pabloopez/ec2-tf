8f716c1a4eb7cb3fdaba31f057f9d2a0d78b5c27
pablopez <pablo.lopezzaldivar@sysdig.com>
2023-08-24 19:28:18 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "compute" {
+  source               = "./module/compute"
+  ami                  = "ami-06db4d78cb1d3bbf9"
+  instance_type        = "m4.xlarge"
+  tag_name             = "ExampleAppServerInstance"
+  sg                   = module.security.webserver_sg
+  user_data            = file("./userdata.tpl")
+  iam_instance_profile = module.iam.s3_profile
+}
+
+module "security" {
+  source = "./module/security"
+}
+
+module "iam" {
+  source                 = "./module/iam"
+  role_name              = "s3-list-bucket"
+  policy_name            = "s3-list-bucket"
+  instance_profile_name  = "s3-list-bucket"
+  path                   = "/"
+  iam_policy_description = "s3 policy for ec2 to list role"
+  iam_policy             = file("./s3-list-bucket-policy.tpl")
+  assume_role_policy     = file("./s3-list-bucket-trusted-identity.tpl")
+}
+module "s3" {
+  source        = "./module/s3"
+  bucket_name   = "devinslevelupintechbucket0223"
+  acl           = "private"
+  object_key    = "LUIT"
+  object_source = "/dev/null"
+}
+
+# we need to run:
+# ssh-keygen -t rsa -b 4096
---
+++ module/security/main.tf
@@ -0,0 +1,35 @@
+# --- modules/security/main.tf
+
+resource "aws_security_group" "webserver_sg" {
+  name        = "HTTP Access via ALB"
+  description = "Allow HTTP inbound traffic via ALB, and SSH traffic via bastion host"
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "project"
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "my_ssh_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
